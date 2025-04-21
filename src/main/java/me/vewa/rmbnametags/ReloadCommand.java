070a1805af7af073ebdc63c624765d7640160a9a
Seva163 <tarassavel2005@gmail.com>
2025-04-21 15:33:59 +0700

    feat: add reload command

---
+++ src/main/java/me/vewa/rmbnametags/ReloadCommand.java
@@ -0,0 +1,20 @@
+package me.vewa.rmbnametags;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class ReloadCommand implements CommandExecutor {
+    private RMBNametags plugin;
+
+    public ReloadCommand(RMBNametags plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        plugin.loadConfig();
+        return true;
+    }
+}
